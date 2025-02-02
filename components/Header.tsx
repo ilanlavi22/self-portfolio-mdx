c354edb9a9afeb6b773a8b83ec71bdce5bca0297
Ilan Lavi <refaelberlin@gmail.com>
2025-02-02 22:07:56 +0100

    update-self-articles

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import Logo from '@/assets/images/logo.svg'
 export default function Header() {
   return (
     <header className='fixed inset-x-0 top-0 z-50 bg-background/75 py-6 backdrop-blur-sm'>
-      <nav className='text-theme-gray-shade font-proxima container flex max-w-3xl items-center justify-between text-[0.9rem] font-bold uppercase leading-[30px] tracking-[0.15rem]'>
+      <nav className='text-theme-gray-shade container flex max-w-3xl items-center justify-between font-proxima text-[0.9rem] font-bold uppercase leading-[30px] tracking-[0.15rem]'>
         <div>
           <Link
             href='/'
@@ -15,17 +15,17 @@ export default function Header() {
             Ilan Lavi
           </Link>
         </div>
-        {/* <ul className='flex items-center gap-6 text-[#B7AB98]/50 sm:gap-10'>
+        <ul className='flex items-center gap-6 text-[#B7AB98]/50 sm:gap-10'>
           <li className='transition-colors hover:text-[#B7AB98]'>
             <Link href='/posts'>Posts</Link>
           </li>
-          <li className='transition-colors hover:text-[#B7AB98]'>
+          {/* <li className='transition-colors hover:text-[#B7AB98]'>
             <Link href='/projects'>Projects</Link>
           </li>
           <li className='transition-colors hover:text-[#B7AB98]'>
             <Link href='/contact'>Contact</Link>
-          </li>
-        </ul> */}
+          </li> */}
+        </ul>
 
         <div>
           <ThemeToggle />
